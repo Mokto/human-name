d19314ab08625003f74dd83fd136dd06a5bd79b3
Theo Mathieu <tmathieu.github@fastmail.com>
2023-01-29 19:04:08 +0100

    chore: proper hashmap 

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use human_name::Name;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn parse_name(name: String) -> PyResult<HashMap<String, Vec<Option<String>>>> {
+fn parse_name(name: String) -> PyResult<HashMap<String, String>> {
     let mut result = HashMap::new();
 
     let n = Name::parse(name.as_str());
@@ -15,31 +15,26 @@ fn parse_name(name: String) -> PyResult<HashMap<String, Vec<Option<String>>>> {
 
     let n = n.unwrap();
 
-    result.insert(
-        "given_name".to_string(),
-        vec![n.given_name().map(str::to_string)],
-    );
-    result.insert(
-        "generational_suffix".to_string(),
-        vec![n.generational_suffix().map(str::to_string)],
-    );
-    result.insert(
-        "honorific_prefix".to_string(),
-        vec![n.honorific_prefix().map(str::to_string)],
-    );
-    result.insert(
-        "honorific_suffix".to_string(),
-        vec![n.honorific_suffix().map(str::to_string)],
-    );
-    result.insert(
-        "middle_name".to_string(),
-        vec![Some(n.middle_name().unwrap_or_default().to_string())],
-    );
-    result.insert("surname".to_string(), vec![Some(n.surname().to_string())]);
-    result.insert(
-        "middle_initials".to_string(),
-        vec![n.middle_initials().map(str::to_string)],
-    );
+    if let Some(given_name) = n.given_name().map(str::to_string) {
+        result.insert("given_name".to_string(), given_name);
+    }
+
+    if let Some(generational_suffix) = n.generational_suffix().map(str::to_string) {
+        result.insert("generational_suffix".to_string(), generational_suffix);
+    }
+    if let Some(honorific_prefix) = n.honorific_prefix().map(str::to_string) {
+        result.insert("honorific_prefix".to_string(), honorific_prefix);
+    }
+    if let Some(honorific_suffix) = n.honorific_suffix().map(str::to_string) {
+        result.insert("honorific_suffix".to_string(), honorific_suffix);
+    }
+    if let Some(middle_initials) = n.middle_initials().map(str::to_string) {
+        result.insert("middle_initials".to_string(), middle_initials);
+    }
+    if let Some(middle_name) = n.middle_name() {
+        result.insert("middle_name".to_string(), middle_name.to_string());
+    }
+    result.insert("surname".to_string(), n.surname().to_string());
 
     Ok(result)
 }
